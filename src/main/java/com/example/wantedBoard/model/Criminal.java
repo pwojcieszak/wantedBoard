9ed38607d7f909897c7cdfe3067e2fddb6b8e91c
Piotr Wojcieszak <piotrekw1002@gmail.com>
2023-06-05 20:33:04 +0200

    Created model 'Criminal'

---
+++ src/main/java/com/example/wantedBoard/model/Criminal.java
@@ -0,0 +1,51 @@
+package com.example.wantedBoard.model;
+
+import java.io.Serializable;
+
+public class Criminal implements Serializable {
+    private String title;
+    private String description;
+    private String caution;
+
+    public Criminal() {
+    }
+
+    public Criminal(String title, String description, String caution) {
+        this.title = title;
+        this.description = description;
+        this.caution = caution;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCaution() {
+        return caution;
+    }
+
+    public void setCaution(String caution) {
+        this.caution = caution;
+    }
+
+    @Override
+    public String toString() {
+        return "Poster{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", caution='" + caution + '\'' +
+                '}';
+    }
+}
