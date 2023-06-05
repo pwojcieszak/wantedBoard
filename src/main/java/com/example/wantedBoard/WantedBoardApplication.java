7eb049196e0f6712226d3318b6a40240c3f8d042
Piotr Wojcieszak <piotrekw1002@gmail.com>
2023-06-05 20:32:25 +0200

    Initial commit of project files

---
+++ src/main/java/com/example/wantedBoard/WantedBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.wantedBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedBoardApplication.class, args);
+	}
+
+}
