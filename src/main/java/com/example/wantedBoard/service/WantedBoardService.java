d585ccdcf8841ded51f33052ea1ad4895d8ba7c5
Piotr Wojcieszak <piotrekw1002@gmail.com>
2023-06-05 22:03:10 +0200

    Created Service for application that uses previously created FBIAPIService

---
+++ src/main/java/com/example/wantedBoard/service/WantedBoardService.java
@@ -0,0 +1,26 @@
+package com.example.wantedBoard.service;
+
+import com.example.wantedBoard.model.Criminal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WantedBoardService {
+
+    private final FBIAPIService fbiApiService;
+
+    @Autowired
+    public WantedBoardService(FBIAPIService fbiApiService) {
+        this.fbiApiService = fbiApiService;
+    }
+
+    public List<Criminal> getAllCriminals() {
+        // Wywołanie metody serwisu FBIAPIService do pobrania danych o przestępcach
+        List<Criminal> criminals = fbiApiService.fetchCriminals();
+
+        return criminals;
+    }
+
+}
